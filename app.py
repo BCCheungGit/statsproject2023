8614bbfbaf495eb96991d39db1d8fdbb3d456ce1
BCCheungGit <87406737+BCCheungGit@users.noreply.github.com>
2023-05-01 11:39:43 +0800

    added some price analysis

--- app.py
+++ app.py
@@ -32,19 +32,6 @@ print('Max value:', vnewdata_max)
 print('Min value:', vnewdata_min)
 print('Median of Volume:', vnewdata_median)
 
-snumericdata = btc_data.astype({'Size(B)' : 'float'})
-snewdata_mean = snumericdata['Size(B)'].mean()
-snewdata_std = snumericdata['Size(B)'].std()
-snewdata_max = snumericdata['Size(B)'].max()
-snewdata_min = snumericdata['Size(B)'].min()
-snewdata_median = snumericdata['Size(B)'].median()
-
-print('Size mean:', snewdata_mean)
-print('Size standard deviation:', snewdata_std)
-print('Size max value:', snewdata_max)
-print('Size min value:', snewdata_min)
-print('Size median:', snewdata_median)
-
 btc_price_data = pd.read_csv('btcprice.csv', 
                              low_memory=False,  
                              names=["Date", 
@@ -53,5 +40,33 @@ btc_price_data = pd.read_csv('btcprice.csv',
                                     "Low",
                                     "Close",
                                     "Adj Close",
-                                    "Volume"])
+                                    "Volume"],skiprows=1)
 
+pricenumericdata_high = btc_price_data.astype({'High' : 'float'})
+high_price_mean = pricenumericdata_high['High'].mean()
+high_price_std = pricenumericdata_high['High'].std()
+high_price_max = pricenumericdata_high['High'].max()
+high_price_min = pricenumericdata_high['High'].min()
+high_price_med = pricenumericdata_high['High'].median()
+
+
+pricenumericdata_low = btc_price_data.astype({'Low' : 'float'})
+low_price_mean = pricenumericdata_low['Low'].mean()
+low_price_std = pricenumericdata_low['Low'].std()
+low_price_max = pricenumericdata_low['Low'].max()
+low_price_min = pricenumericdata_low['Low'].min()
+low_price_med = pricenumericdata_low['Low'].median()
+
+
+print("Average high price:", high_price_mean)
+print("High standard deviation:", high_price_std)
+print("High max price:", high_price_max)
+print("High min price:", high_price_min)
+print("Median high price:", high_price_med)
+
+
+print("Average low price:", low_price_mean)
+print("Low standard deviation:", low_price_std)
+print("Low max price:", low_price_max)
+print("Low min price:", low_price_min)
+print("Median low price:", low_price_med)
