dd7e47230aac1a992fad3cc02a95187a1aadf44f
BCCheungGit <87406737+BCCheungGit@users.noreply.github.com>
2023-05-01 11:41:44 +0800

    added comments for readability

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pickle as pk
 
-
+#read from prof zhangs data, using only data after 2018
 btc_data = pd.read_csv('btc.csv', 
                        low_memory=False, 
                        thousands=',', 
@@ -19,6 +19,7 @@ btc_data = pd.read_csv('btc.csv',
                               "BlocK Reward_Tips", 
                               "Time"])
 
+#perform basic analysis of the volume
 vnumericdata = btc_data.astype({'Volume': 'float'})
 vnewdata_mean = vnumericdata['Volume'].mean()
 vnewdata_std = vnumericdata['Volume'].std()
@@ -26,12 +27,15 @@ vnewdata_max = vnumericdata['Volume'].max()
 vnewdata_min = vnumericdata['Volume'].min()
 vnewdata_median = vnumericdata['Volume'].median()
 
+#print the volume analysis information
 print('Volume mean:', vnewdata_mean)
 print('Standard deviation:', vnewdata_std)
 print('Max value:', vnewdata_max)
 print('Min value:', vnewdata_min)
 print('Median of Volume:', vnewdata_median)
 
+
+#read from the bitcoin price data (taken from Yahoo Finance)
 btc_price_data = pd.read_csv('btcprice.csv', 
                              low_memory=False,  
                              names=["Date", 
@@ -42,6 +46,7 @@ btc_price_data = pd.read_csv('btcprice.csv',
                                     "Adj Close",
                                     "Volume"],skiprows=1)
 
+#Analyze high data
 pricenumericdata_high = btc_price_data.astype({'High' : 'float'})
 high_price_mean = pricenumericdata_high['High'].mean()
 high_price_std = pricenumericdata_high['High'].std()
@@ -49,7 +54,7 @@ high_price_max = pricenumericdata_high['High'].max()
 high_price_min = pricenumericdata_high['High'].min()
 high_price_med = pricenumericdata_high['High'].median()
 
-
+#Analyze Low data
 pricenumericdata_low = btc_price_data.astype({'Low' : 'float'})
 low_price_mean = pricenumericdata_low['Low'].mean()
 low_price_std = pricenumericdata_low['Low'].std()
@@ -58,13 +63,14 @@ low_price_min = pricenumericdata_low['Low'].min()
 low_price_med = pricenumericdata_low['Low'].median()
 
 
+#print high data
 print("Average high price:", high_price_mean)
 print("High standard deviation:", high_price_std)
 print("High max price:", high_price_max)
 print("High min price:", high_price_min)
 print("Median high price:", high_price_med)
 
-
+#print low data
 print("Average low price:", low_price_mean)
 print("Low standard deviation:", low_price_std)
 print("Low max price:", low_price_max)
